998208be846b1e0d8c2fd6c3e2be720fb2f743ff
Lucas Ferrauto <ferrauto3u@etu.univ-lorraine.fr>
2022-11-25 17:17:30 +0100

    more_attributes

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 var board = document.getElementById("board");
+var page = document.getElementById("page");
 console.log("hello")
 
-for (var i = 0 ; i < 9; i ++){
+for (var i = 0 ; i < 1; i ++){
+
     var cells = document.createElement("use");
-   
-    cells.setAttribute("href", "#board");
+
+    cells.setAttribute("href", "#board");    
+    cells.setAttribute("width", "50");  
+    cells.setAttribute("height" , "50");
+    cells.setAttribute("transform", "translate(0  0)");      
+
     cells.id = `case${i}`;
     cells.innerHTML="";
-   
-    cells.width="50";
-    cells.height="50";
-    console.log(cells)
+    page.appendChild(cells);
+    console.log(cells);
+  
+
+}
+
+console.log("oui")
+document.querySelectorAll("use[href='#board']").forEach(element => {       
+                
+    element.addEventListener("click", e => {
+        console.log("e id : " , e.target.attributes.id)
+
+        
+    })
+}) 
+function tick(e){
+    
 
 }
